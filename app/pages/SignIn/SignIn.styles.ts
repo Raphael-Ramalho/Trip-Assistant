63c7c9cffd3b17b55f3fea69841afb7cbd0ad68e
Raphael-Ramalho <rsramalhovf@gmail.com>
2024-07-22 21:05:37 -0300

    add sign in page

---
+++ app/pages/SignIn/SignIn.styles.ts
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+
+import Theme from "@/theme/Theme";
+
+const { colors } = Theme;
+
+
+
+const formStyles = StyleSheet.create({
+  emailContainer: {
+    marginTop: 50,
+  },
+  input: {
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: colors.gray,
+    fontFamily: "outfit",
+  },
+  inputTitle: {
+    fontFamily: "outfit",
+  },
+})
+
+const buttonsStyle = StyleSheet.create({
+  /* Sign in button */
+  signInButton: {
+    padding: 15,
+    backgroundColor: colors.primary,
+    borderRadius: 15,
+    marginTop: 50,
+  },
+  signInText: {
+    color: colors.white,
+    textAlign: "center",
+  },
+  /* Create account button */
+  createAccountButton: {
+    padding: 13,
+    backgroundColor: colors.white,
+    borderRadius: 15,
+    marginTop: 20,
+    borderWidth: 1
+  },
+  createAccountText: {
+    color: colors.primary,
+    textAlign: "center",
+  },
+})
+
+export const signInStyles = StyleSheet.create({
+  ...formStyles,
+  ...buttonsStyle,
+  container: {
+    backgroundColor: colors.white,
+    height: "100%",
+    padding: 25,
+    paddingTop: 80,
+  },
+  mainText: {
+    fontFamily: "outfitBold",
+    fontSize: 30,
+  },
+  secondaryText: {
+    fontFamily: "outfit",
+    fontSize: 30,
+    color: colors.gray,
+    marginTop: 20,
+  },
+  terciaryText: {
+    fontFamily: "outfit",
+    fontSize: 26,
+    color: colors.gray,
+    marginTop: 10,
+  },
+});
--- app/pages/SignIn/SignIn.tsx
+++ app/pages/SignIn/SignIn.tsx
@@ -1,9 +1,60 @@
-import { Text, View } from "react-native";
+import { signInStyles } from "@/pages/SignIn/SignIn.styles";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { Text, TextInput, View } from "react-native";
+
+const {
+  container,
+  createAccountButton,
+  createAccountText,
+  emailContainer,
+  input,
+  inputTitle,
+  mainText,
+  secondaryText,
+  signInButton,
+  signInText,
+  terciaryText,
+} = signInStyles;
 
 export default function SignIn() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
   return (
-    <View>
-      <Text>Sign In</Text>
+    <View style={container}>
+      <Text style={mainText}>Let's Sign You In</Text>
+      <Text style={secondaryText}>Welcome Back</Text>
+      <Text style={terciaryText}>You've been missed!</Text>
+
+      {/* Email */}
+      <View style={emailContainer}>
+        <Text style={inputTitle}>Email</Text>
+        <TextInput placeholder="Enter Email" style={input} />
+      </View>
+
+      {/* Password */}
+      <View style={emailContainer}>
+        <Text style={inputTitle}>Password</Text>
+        <TextInput
+          placeholder="Enter Password"
+          secureTextEntry={true}
+          style={input}
+        />
+      </View>
+
+      {/* Sign In Button */}
+      <View style={signInButton}>
+        <Text style={signInText}>Sign In</Text>
+      </View>
+
+      {/* Create Account Button */}
+      <View style={createAccountButton}>
+        <Text style={createAccountText}>Create Account</Text>
+      </View>
     </View>
   );
 }
