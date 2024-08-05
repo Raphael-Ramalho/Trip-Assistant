66d9242f45a9c1803a6e1618c0954f10027ffa7b
Raphael-Ramalho <rsramalhovf@gmail.com>
2024-08-05 12:17:07 -0300

    created sign in page

---
+++ app/pages/SignUp/SignUp.styles.ts
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+import Theme from "@/theme/Theme";
+
+const { colors } = Theme;
+
+export const createAccountStyle = StyleSheet.create({
+  container: {
+    padding: 25,
+    paddingTop: 80,
+    backgroundColor: colors.white,
+    height: "100%",
+  },
+  text: {
+    fontFamily: "outfitBold",
+    fontSize: 30,
+  },
+});
+
+export const formStyles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 20,
+  },
+  input: {
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: colors.gray,
+    fontFamily: "outfit",
+  },
+  inputTitle: {
+    fontFamily: "outfit",
+  },
+});
+
+export const buttonsStyle = StyleSheet.create({
+  /* Sign in button */
+  signInButton: {
+    padding: 20,
+    backgroundColor: colors.primary,
+    borderRadius: 15,
+    marginTop: 50,
+  },
+  signInText: {
+    color: colors.white,
+    textAlign: "center",
+  },
+  /* Create account button */
+  createAccountButton: {
+    padding: 18,
+    backgroundColor: colors.white,
+    borderRadius: 15,
+    marginTop: 20,
+    borderWidth: 1,
+  },
+  createAccountText: {
+    color: colors.primary,
+    textAlign: "center",
+  },
+});
--- app/pages/SignUp/SignUp.tsx
+++ app/pages/SignUp/SignUp.tsx
@@ -1,9 +1,63 @@
-import { Text, View } from "react-native";
+import {
+  buttonsStyle,
+  createAccountStyle,
+  formStyles,
+} from "@/pages/SignUp/SignUp.styles";
+import { useNavigation, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+const { text, container } = createAccountStyle;
+const { inputContainer, input, inputTitle } = formStyles;
+const { createAccountButton, createAccountText, signInButton, signInText } =
+  buttonsStyle;
 
 export default function SignUp() {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
   return (
-    <View>
-      <Text>Sign Up</Text>
+    <View style={container}>
+      <Text style={text}>Create New Account</Text>
+
+      {/* User Full Name */}
+      <View style={inputContainer}>
+        <Text style={inputTitle}>Full Name</Text>
+        <TextInput placeholder="Enter Full Name" style={input} />
+      </View>
+
+      {/* Email */}
+      <View style={inputContainer}>
+        <Text style={inputTitle}>Email</Text>
+        <TextInput placeholder="Enter Email" style={input} />
+      </View>
+
+      {/* Password */}
+      <View style={inputContainer}>
+        <Text style={inputTitle}>Password</Text>
+        <TextInput
+          placeholder="Enter Password"
+          secureTextEntry={true}
+          style={input}
+        />
+      </View>
+
+      {/* Sign In Button */}
+      <TouchableOpacity style={signInButton}>
+        <Text style={signInText}>Create Account</Text>
+      </TouchableOpacity>
+
+      {/* Create Account Button */}
+      <TouchableOpacity
+        style={createAccountButton}
+        onPress={() => router.replace("pages/SignIn/SignIn")}
+      >
+        <Text style={createAccountText}>Sign In</Text>
+      </TouchableOpacity>
     </View>
   );
 }
