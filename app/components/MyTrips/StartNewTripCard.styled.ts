1622d63e9e501c90716b885dcc2455876e58452e
Raphael De Souza Ramalho <rsramalhovf@gmail.com>
2024-09-11 11:35:43 -0300

    add trips page

---
+++ app/components/MyTrips/StartNewTripCard.styled.ts
@@ -0,0 +1,36 @@
+import { StyleSheet, TextStyle } from "react-native";
+
+import Theme from "@/theme/Theme";
+
+const { colors } = Theme;
+
+export const cardStyles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginTop: 50,
+    display: "flex",
+    alignItems: "center",
+    gap: 25,
+  },
+  label: {
+    fontSize: 25,
+    fontFamily: "outfitMedium",
+  },
+  underLabel: {
+    fontSize: 25,
+    fontFamily: "outfit",
+    textAlign: "center",
+    color: colors.gray,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: colors.primary,
+    borderRadius: 15,
+    paddingHorizontal: 30,
+  },
+  buttonText: {
+    color: colors.white,
+    fontFamily: "outfitMedium",
+    fontSize: 17,
+  },
+});
--- app/components/MyTrips/StartNewTripCard.tsx
+++ app/components/MyTrips/StartNewTripCard.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { cardStyles } from "./StartNewTripCard.styled";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useTranslation } from "react-i18next";
 
 function StartNewTripCard() {
+  const { t } = useTranslation();
+
+  const cardPrefix = "components.card";
+
   return (
-    <View>
-      <Text> ola</Text>
+    <View style={cardStyles.container}>
+      <Ionicons name="location-sharp" size={30} color="black" />
+      <Text style={cardStyles.label}>{t(`${cardPrefix}.no-trips`)}</Text>
+
+      <Text style={cardStyles.underLabel}>
+        {t(`${cardPrefix}.no-trips-under-label"`)}
+      </Text>
+
+      <TouchableOpacity style={cardStyles.button}>
+        <Text style={cardStyles.buttonText}>{t(`${cardPrefix}.button`)}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
